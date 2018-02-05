3d990c7d58a69670b30c7f60208f0b40d9df9d76
Azmul <azmulg@gmail.com>
2018-02-05 21:33:56 +0600

    add burger ingredients daynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 import classes from './Burger.css';
 
 const Burger = (props) =>{
+   
+   const ingredients = Object.keys(props.ingredients).map(igKey =>{
+       return [...Array(props.ingredients[igKey])].map((_, index) =>{
+           return <BurgerIngredient key={igKey+index} type={igKey} />
+       })
+   });
+   console.log(ingredients);
+   
    return(
        <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+               {ingredients}
             <BurgerIngredient type="bread-bottom" />
        </div>
    )
